fcd42a94d3826acf56525d5f97b9ef969d04c4ae
mostafamohm2002ead@gmail.com <mostafamohm2002ead@gmail.com>
2025-10-01 13:45:39 +0300

    Add project files.

---
+++ CodingWiki.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CodingWiki.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
